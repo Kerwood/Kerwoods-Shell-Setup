62344dd8945580f7933e7d124d5ad643f3de2c63
Patrick Kerwood <patrick@kerwood.dk>
2016-01-23 08:08:17 +0100

    Uploaded

--- shell-setup.sh
+++ shell-setup.sh
@@ -19,40 +19,57 @@ echo -n "${GREEN} [+] Installing git, zsh, tmux, vim, curl${NORMAL}"
 sudo apt-get -y install git zsh tmux vim curl > /dev/null
 
 if [ $? -eq 0 ]; then
-echo -n "${GREEN} .. Done!${NORMAL}"
-
+	echo "${GREEN} .. Done!${NORMAL}"
+else
+	echo "${RED} .. Failed!${NORMAL}"
 fi
 
 echo "${GREEN} [+] Downloading tmux.conf${NORMAL}"
-curl -o ~/.tmux.conf https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Tmux/.tmux.conf > /dev/null
+curl -o ~/.tmux.conf https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Tmux/.tmux.conf > /dev/null 2&>1
 
 
-echo "${GREEN} [+] Installing tmux-mem-cpu-load${NORMAL}"
+echo -n "${GREEN} [+] Installing tmux-mem-cpu-load${NORMAL}"
 
-sudo apt-get -y install cmake g++ # > /dev/null
-git clone https://github.com/thewtex/tmux-mem-cpu-load.git # > /dev/null
+sudo apt-get -y install cmake g++ > /dev/null
+git clone https://github.com/thewtex/tmux-mem-cpu-load.git > /dev/null
 cd tmux-mem-cpu-load
-cmake . # > /dev/null
-make # > /dev/null
-sudo make install # > /dev/null
+cmake . > /dev/null
+make > /dev/null
+sudo make install > /dev/null
+
+if [ $? -eq 0 ]; then
+	echo "${GREEN} .. Done!${NORMAL}"
+else
+	echo "${RED} .. Failed!${NORMAL}"
+fi
+
 cd ..
 rm -rf tmux-mem-cpu-load
 
 
-echo "${GREEN} [+] Downloading Vim configuraiton files${NORMAL}"
+echo -n "${GREEN} [+] Downloading Vim configuraiton files${NORMAL}"
 mkdir -p ~/.vim/colors
-curl -o ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/monokai.vim > /dev/null
-curl -o ~/.vimrc https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/.vimrc > /dev/null
+curl -o ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/monokai.vim > /dev/null 2&>1
+curl -o ~/.vimrc https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/.vimrc > /dev/null 2&>1
 
+if [ $? -eq 0 ]; then
+	echo "${GREEN} .. Done!${NORMAL}"
+else
+	echo "${RED} .. Failed!${NORMAL}"
+fi
 
-echo "${GREEN} [+] Installing Oh My Zsh${NORMAL}"
-
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" | exit
-curl -o ~/.oh-my-zsh/themes/kerwood.zsh-theme https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Oh-my-zsh/kerwood.zsh-theme > /dev/null
+echo -n "${GREEN} [+] Installing Oh My Zsh${NORMAL}"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -o ~/.oh-my-zsh/themes/kerwood.zsh-theme https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Oh-my-zsh/kerwood.zsh-theme > /dev/null 2&>1
 sed -i -e "s/robbyrussell/kerwood/g" ~/.zshrc
 
-echo "${GREEN} [+] Enableing tmux auto start${NORMAL}"
+if [ $? -eq 0 ]; then
+	echo "${GREEN} .. Done!${NORMAL}"
+else
+	echo "${RED} .. Failed!${NORMAL}"
+fi
+
+echo -n "${GREEN} [+] Enableing tmux auto start${NORMAL}"
 cat >> ~/.zshrc << \EOF
 
 # Start Tmux at login
@@ -60,6 +77,12 @@ if [[ $(echo $TTY | cut -d \/ -f 4) = '0' ]]; then
 	tmux -2 a || tmux -2
 fi
 EOF
+
+if [ $? -eq 0 ]; then
+	echo "${GREEN} .. Done!${NORMAL}"
+else
+	echo "${RED} .. Failed!${NORMAL}"
+fi
 
 echo
 echo "${GREEN} [+] Done!..${NORMAL}"
