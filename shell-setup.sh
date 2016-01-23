6563a746795fa7c882f1ffdf0a5ce4f036cd3568
Patrick Kerwood <patrick@kerwood.dk>
2016-01-23 07:59:16 +0100

    Uploaded

--- shell-setup.sh
+++ shell-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 clear
 echo
 echo "Kerwood's Shell Setup"
@@ -12,38 +13,43 @@ RED=$(tput setaf 1)
 PURPLE=$(tput setaf 5)
 
 echo "${GREEN} [+] Updating apt-get${NORMAL}"
-sudo apt-get update > /dev/null 2>&1
+sudo apt-get update > /dev/null
 
-echo "${GREEN} [+] Installing git, zsh, tmux, vim, curl${NORMAL}"
-sudo apt-get -y install git zsh tmux vim curl > /dev/null 2>&1
+echo -n "${GREEN} [+] Installing git, zsh, tmux, vim, curl${NORMAL}"
+sudo apt-get -y install git zsh tmux vim curl > /dev/null
+
+if [ $? -eq 0 ]; then
+echo -n "${GREEN} .. Done!${NORMAL}"
+
+fi
 
 echo "${GREEN} [+] Downloading tmux.conf${NORMAL}"
-curl -o ~/.tmux.conf https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Tmux/.tmux.conf > /dev/null 2>&1
+curl -o ~/.tmux.conf https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Tmux/.tmux.conf > /dev/null
 
 
 echo "${GREEN} [+] Installing tmux-mem-cpu-load${NORMAL}"
 
-sudo apt-get -y install cmake g++ > /dev/null 2>&1
-git clone https://github.com/thewtex/tmux-mem-cpu-load.git > /dev/null 2>&1
+sudo apt-get -y install cmake g++ > /dev/null
+git clone https://github.com/thewtex/tmux-mem-cpu-load.git > /dev/null
 cd tmux-mem-cpu-load
-cmake . > /dev/null 2>&1
-make > /dev/null 2>&1
-sudo make install > /dev/null 2>&1
+cmake . > /dev/null
+make > /dev/null
+sudo make install > /dev/null
 cd ..
 rm -rf tmux-mem-cpu-load
 
 
 echo "${GREEN} [+] Downloading Vim configuraiton files${NORMAL}"
 mkdir -p ~/.vim/colors
-curl -o ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/monokai.vim > /dev/null 2>&1
-curl -o ~/.vimrc https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/.vimrc > /dev/null 2>&1
+curl -o ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/monokai.vim > /dev/null
+curl -o ~/.vimrc https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Vim/.vimrc > /dev/null
 
 
 echo "${GREEN} [+] Installing Oh My Zsh${NORMAL}"
 
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" | exit
-curl -o ~/.oh-my-zsh/themes/kerwood.zsh-theme https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Oh-my-zsh/kerwood.zsh-theme > /dev/null 2>&1
+curl -o ~/.oh-my-zsh/themes/kerwood.zsh-theme https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Oh-my-zsh/kerwood.zsh-theme > /dev/null
 sed -i -e "s/robbyrussell/kerwood/g" ~/.zshrc
 
 echo "${GREEN} [+] Enableing tmux auto start${NORMAL}"
