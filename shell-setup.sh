a3e9e863345e0afcf4d51832dd37d1b3b4ae3439
Kerwood <patrick@kerwood.dk>
2016-10-11 22:16:26 +0200

    Update shell-setup.sh

--- shell-setup.sh
+++ shell-setup.sh
@@ -25,16 +25,16 @@ echo "${GREEN} [+] Downloading tmux.conf${NORMAL}"
 curl -o ~/.tmux.conf https://raw.githubusercontent.com/Kerwood/Kerwoods-Shell-Setup/master/Tmux/.tmux.conf > /dev/null 2>&1
 
 echo "${GREEN} [+] Installing Vim Docker Syntax${NORMAL}"
-git clone https://github.com/ekalinin/Dockerfile.vim.git
-cd Dockerfile.vim
-make install
+git clone https://github.com/ekalinin/Dockerfile.vim.git > /dev/null
+cd Dockerfile.vim > /dev/null
+make install > /dev/null
 cd ..
-rm -rvf Dockerfile.vim
+rm -rvf Dockerfile.vim > /dev/null
 
 echo "${GREEN} [+] Installing tmux-mem-cpu-load${NORMAL}"
 
 sudo apt-get -y install cmake g++ > /dev/null
-git clone -q https://github.com/thewtex/tmux-mem-cpu-load.git
+git clone -q https://github.com/thewtex/tmux-mem-cpu-load.git > /dev/null
 cd tmux-mem-cpu-load
 cmake . > /dev/null
 make > /dev/null
